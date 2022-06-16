f731efdf8ac10996d1fa33cfdda608f68e0a7c50
Waqas Azeem <waqas.azeem@emirates.com>
2022-06-16 21:55:54 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const {add, subtract,multiply,divide} = require('./math');
+
+
+console.log('Hello world!!!');
+console.log(add(12,10));
+console.log(subtract(12,10));
+console.log(multiply(12,10));
+console.log(divide(12,10));
